9cf395135c85a7b3bbf365ecb1f1b15d32b38ce0
CodeCLS <primocaleb@gmail.com>
2022-05-07 20:34:24 +0200

    make it bettr

--- src/main/java/carlo/api/ApiApplication.java
+++ src/main/java/carlo/api/ApiApplication.java
@@ -22,7 +22,7 @@ public class ApiApplication {
     public DeferredResult<String> exchangeAuthCode(@RequestParam(value = "code", defaultValue = "null") String code) throws Exception {
         DeferredResult<String> output = new DeferredResult<>();
         // Setup
-        String clientId = "3b683bb7-48a3-4b4c-8a2f-7337a8a0ee19\n";
+        String clientId = "3b683bb7-48a3-4b4c-8a2f-7337a8a0ee19";
         String clientSecret = "bdefd1ee-de23-4df5-aa61-c9ef1d6ac724";
         String redirectUri = "sc3b683bb7-48a3-4b4c-8a2f-7337a8a0ee19://myapp.com/callback";
         String[] scope = {"read_vehicle_info", "read_odometer"};
