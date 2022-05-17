db5bd460f3611eb4756264d151c5be9859d079c0
CodeCLS <primocaleb@gmail.com>
2022-05-17 20:31:51 +0200

    adding endpoints

--- src/main/java/carlo/api/ApiApplication.java
+++ src/main/java/carlo/api/ApiApplication.java
@@ -1,10 +1,10 @@
 package carlo.api;
 
+import com.google.gson.JsonObject;
 import com.smartcar.sdk.AuthClient;
 import com.smartcar.sdk.Smartcar;
 import com.smartcar.sdk.Vehicle;
-import com.smartcar.sdk.data.Auth;
-import com.smartcar.sdk.data.VehicleIds;
+import com.smartcar.sdk.data.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
@@ -57,18 +57,116 @@ public class ApiApplication {
 
     }
 
-    @GetMapping("/attributes")
-    public DeferredResult<String> attributes(@RequestParam(value = "code", defaultValue = "null") String code) throws Exception {
-        DeferredResult<String> output = new DeferredResult<>();
-        // Setup
+    @GetMapping("/location")
+    public JsonObject location(@RequestParam(value = "code", defaultValue = "null") String code) throws Exception {
         VehicleIds response = Smartcar.getVehicles(code);
         String[] vehicleIds = response.getVehicleIds();
         Vehicle vehicle =new Vehicle(vehicleIds[0],code);
-        output.setResult("{"+ vehicle+ " }");
+        JsonObject jsonObject = new JsonObject();
+        VehicleLocation location = vehicle.location();
+        jsonObject.addProperty("latitude",""+location.getLatitude());
+        jsonObject.addProperty("longitude",location.getLongitude());
 
-        return output;
 
+        return jsonObject;
     }
+    @GetMapping("/vehicle_info")
+    public JsonObject vehicleInformation(@RequestParam(value = "code", defaultValue = "null") String code) throws Exception {
+        VehicleIds response = Smartcar.getVehicles(code);
+        String[] vehicleIds = response.getVehicleIds();
+        Vehicle vehicle =new Vehicle(vehicleIds[0],code);
+        JsonObject jsonObject = new JsonObject();
+        VehicleAttributes vehicleInfo = vehicle.attributes();
+        String vehicleId = vehicleInfo.getId();
+        String vehicleMake = vehicleInfo.getMake();
+        String vehicleYear = ""+vehicleInfo.getYear();
+        String vehicleModel = vehicleInfo.getModel();
+
+        jsonObject.addProperty("vehicleId",""+vehicleId);
+        jsonObject.addProperty("vehicleMake",vehicleMake);
+        jsonObject.addProperty("vehicleYear",""+vehicleYear);
+        jsonObject.addProperty("vehicleModel",vehicleModel);
+        return jsonObject;
+    }
+    @GetMapping("/odometer")
+    public JsonObject odometer(@RequestParam(value = "code", defaultValue = "null") String code) throws Exception {
+        VehicleIds response = Smartcar.getVehicles(code);
+        String[] vehicleIds = response.getVehicleIds();
+        Vehicle vehicle =new Vehicle(vehicleIds[0],code);
+        JsonObject jsonObject = new JsonObject();
+        VehicleOdometer odometer = vehicle.odometer();
+
+        jsonObject.addProperty("odometer",""+odometer.getDistance());
+
+        return jsonObject;
+    }
+    @GetMapping("/battery")
+    public JsonObject battery(@RequestParam(value = "code", defaultValue = "null") String code) throws Exception {
+        VehicleIds response = Smartcar.getVehicles(code);
+        String[] vehicleIds = response.getVehicleIds();
+        Vehicle vehicle =new Vehicle(vehicleIds[0],code);
+        JsonObject jsonObject = new JsonObject();
+        VehicleBattery battery = vehicle.battery();
+
+        jsonObject.addProperty("battery_percent",""+battery.getPercentRemaining());
+        jsonObject.addProperty("battery_range",""+battery.getRange());
+
+        return jsonObject;
+    }
+    @GetMapping("/lock")
+    public JsonObject lock(@RequestParam(value = "code", defaultValue = "null") String code) throws Exception {
+        VehicleIds response = Smartcar.getVehicles(code);
+        String[] vehicleIds = response.getVehicleIds();
+        Vehicle vehicle =new Vehicle(vehicleIds[0],code);
+        JsonObject jsonObject = new JsonObject();
+        ActionResponse lock = vehicle.lock();
+
+        jsonObject.addProperty("lock_status",""+lock.getStatus());
+        jsonObject.addProperty("lock_msg",""+lock.getStatus());
+
+        return jsonObject;
+    }
+    @GetMapping("/unlock")
+    public JsonObject unlock(@RequestParam(value = "code", defaultValue = "null") String code) throws Exception {
+        VehicleIds response = Smartcar.getVehicles(code);
+        String[] vehicleIds = response.getVehicleIds();
+        Vehicle vehicle =new Vehicle(vehicleIds[0],code);
+        JsonObject jsonObject = new JsonObject();
+        ActionResponse unlock = vehicle.unlock();
+
+        jsonObject.addProperty("unlock_status",""+unlock.getStatus());
+        jsonObject.addProperty("unlock_msg",""+unlock.getStatus());
+
+        return jsonObject;
+    }
+    @GetMapping("/fuel")
+    public JsonObject fuel(@RequestParam(value = "code", defaultValue = "null") String code) throws Exception {
+        VehicleIds response = Smartcar.getVehicles(code);
+        String[] vehicleIds = response.getVehicleIds();
+        Vehicle vehicle =new Vehicle(vehicleIds[0],code);
+        JsonObject jsonObject = new JsonObject();
+        VehicleFuel fuel = vehicle.fuel();
+
+        jsonObject.addProperty("fuel_percent",""+fuel.getPercentRemaining());
+        jsonObject.addProperty("fuel_range",""+fuel.getRange());
+        jsonObject.addProperty("fuel_amount",""+fuel.getAmountRemaining());
+
+        return jsonObject;
+    }
+    @GetMapping("/charge")
+    public JsonObject charge(@RequestParam(value = "code", defaultValue = "null") String code) throws Exception {
+        VehicleIds response = Smartcar.getVehicles(code);
+        String[] vehicleIds = response.getVehicleIds();
+        Vehicle vehicle =new Vehicle(vehicleIds[0],code);
+        JsonObject jsonObject = new JsonObject();
+        VehicleCharge charge = vehicle.charge();
+
+        jsonObject.addProperty("charge_state",""+charge.getState());
+        jsonObject.addProperty("charge_plugged_in",""+charge.getIsPluggedIn());
+
+        return jsonObject;
+    }
+
     //@RequestMapping(
     //        value = CONSTANT_MESSAGE_SEND_URL,
     //        method = RequestMethod.POST)
