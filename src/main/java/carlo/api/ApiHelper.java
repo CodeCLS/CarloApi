d17fff24fe8aff86b13efb876ec5cc9fbedb47cc
CodeCLS <primocaleb@gmail.com>
2022-07-19 08:53:11 +0900

    added new code

--- src/main/java/carlo/api/ApiHelper.java
+++ src/main/java/carlo/api/ApiHelper.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class ApiHelper {
-    private ArrayList<String> apiCodes = new ArrayList<String>(Collections.singletonList("oaiu3490S=?n´´03´´ß094ßn´´0"));
+    private ArrayList<String> apiCodes = new ArrayList<String>(Collections.singletonList("oaiu3490S=?n´´03´´ß094ßn´´0","oaiu3490S=?nD2103_qß094ßn-+0"));
 
     public boolean isValid(String apiCode) {
         return apiCodes.contains(apiCode);
