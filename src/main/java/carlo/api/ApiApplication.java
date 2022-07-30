7feffe87ef44367bbe3ddf2d3594dcdf38100a9c
CodeCLS <primocaleb@gmail.com>
2022-07-30 19:44:26 +0900

    working on refresh endpoint II

--- src/main/java/carlo/api/ApiApplication.java
+++ src/main/java/carlo/api/ApiApplication.java
@@ -184,7 +184,7 @@ public class ApiApplication {
 
         return result;
     }
-    @RequestMapping(value = "/user/{uid}/refresh/",method = RequestMethod.POST)
+    @PostMapping(value = "/user/{uid}/refresh/")
     public DeferredResult<String> refresh(
             @RequestHeader("api-code") String apiCode,
             @RequestHeader("access-token-smart-car") String token,
